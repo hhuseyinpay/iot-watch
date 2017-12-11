a4b0e7183adb22dbf71a6b625a6e786b8f74cd0a
Hasan Hüseyin Pay <h.huseyinpay@gmail.com>
2017-12-11 22:25:39 +0300

    create queries and test

--- manage.py
+++ manage.py
@@ -17,8 +17,83 @@ def createdb():
     try:
         cur = conn.cursor()
 
-        cur.execute(
-            "CREATE TABLE users (ssn INTEGER PRIMARY KEY,first_name VARCHAR(255) NOT NULL,last_name VARCHAR(255) NOT NULL,user_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL)")
+        # user
+        cur.execute("""
+            CREATE TABLE users (
+            ssn INTEGER PRIMARY KEY,
+            first_name VARCHAR(255) NOT NULL,
+            last_name VARCHAR(255) NOT NULL,
+            user_name VARCHAR(255) NOT NULL, 
+            password VARCHAR(255) NOT NULL, 
+            description VARCHAR(255) NOT NULL)""")
+
+        # admin
+        cur.execute("""
+        CREATE TABLE admins (
+        SSN INT NOT NULL,
+        first_name VARCHAR(40) NOT NULL,
+        last_name VARCHAR(40) NOT NULL,
+        user_name VARCHAR(40) NOT NULL,
+        password VARCHAR(40) NOT NULL,
+        description VARCHAR(100),
+        PRIMARY KEY(SSN)
+        )""")
+
+        # location
+        cur.execute("""
+        CREATE TABLE location (
+        id INT NOT NULL,
+        name VARCHAR(40) NOT NULL,
+        description VARCHAR(100),
+        PRIMARY KEY(id)
+        )""")
+
+        # measurement_type
+        cur.execute("""
+        CREATE TABLE measurement_type (
+        id INT NOT NULL,
+        name VARCHAR(40) NOT NULL,
+        description VARCHAR(100),
+        PRIMARY KEY(id)
+        )""")
+
+        # device_type
+        cur.execute("""
+        CREATE TABLE device_type (
+        id INT NOT NULL,
+        name VARCHAR(40) NOT NULL,
+        description VARCHAR(100),
+        PRIMARY KEY(id)
+        )""")
+
+        # reporting_device
+        cur.execute("""
+        CREATE TABLE reporting_device (
+        id INT NOT NULL,
+        name VARCHAR(40) NOT NULL,
+        description VARCHAR(100),
+        lastIpaddress VARCHAR(50),
+        device_type_id INT NOT NULL,
+        location_id INT NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY(device_type_id) REFERENCES device_type(id),
+        FOREIGN KEY(location_id) REFERENCES location(id)
+        )""")
+
+        # measurrment
+        cur.execute("""
+        CREATE TABLE measurement (
+        id INT NOT NULL,
+        measurement_type_id INT NOT NULL,
+        reporting_device_id INT NOT NULL,
+        location_id INT NOT NULL,
+        measured_value FLOAT,
+        measured_date TIMESTAMP,
+        PRIMARY KEY(id),
+        FOREIGN KEY(measurement_type_id) REFERENCES measurement_type(id),
+        FOREIGN KEY(reporting_device_id) REFERENCES reporting_device(id),
+        FOREIGN KEY(location_id) REFERENCES location(id)
+        )""")
 
         conn.commit()
         cur.close()
@@ -39,18 +114,18 @@ def dropdb():
 @manager.command
 def fakedata():
     from app.db.models.user import UserModel
-    from app.db.queries.user import UserQueries
+    from app.db.business_logic.user import UserBusinessLogic
 
     user = UserModel()
 
-    user.ssn = "1111234"
+    user.ssn = "1111"
     user.username = "test_username"
     user.password = "test_password"
     user.firstname = "test_firstname"
     user.lastname = "test_lasname"
     user.description = "test_description"
 
-    UserQueries.insert(user)
+    UserBusinessLogic.create(user)
 
     # rows = FUsers.select()
     """
@@ -62,6 +137,13 @@ def fakedata():
         print("PASSWORD = ", row[4])
         print("DESCRIPTON = ", row[5], "\n")
     """
+
+
+@manager.command
+def get():
+    from app.db.queries.user import UserQueries
+
+    print(UserQueries.get_user(1111).firstname)
 
 
 if __name__ == '__main__':
